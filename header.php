0337a4cd96b84baa17ba981ef27b957fe7f528a8
Cameron Banowsky <cameron@hack.cd>
2015-11-18 15:00:22 -0800

    analytics

--- header.php
+++ header.php
@@ -66,6 +66,16 @@ WebFontConfig = { fontdeck: { id: '57962' } };
   s.parentNode.insertBefore(wf, s);
 })();
 </script -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70348911-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <div id="outer-wrap">
 <div id="inner-wrap1">
     <div id="pageslide">
