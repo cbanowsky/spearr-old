2649566a8e622447a084940dc1b2c43060e41b59
Cameron Banowsky <cameron@hack.cd>
2015-12-28 09:22:41 -0800

    remove gallery column

--- inc/gallery-post-type.php
+++ inc/gallery-post-type.php
@@ -3,10 +3,10 @@
 /* Register Custom Taxonomies */
 
 //// FUNCTION TO CREATE IT
-function unpress_gallery_register() {  
+function unpress_gallery_register() {
 
 	global $nt_option;
-	
+
 	//// LABELS
 	$labels = array(
 		'name' => __('Galleries', 'favethemes'),
@@ -23,38 +23,38 @@ function unpress_gallery_register() {
 			'parent_item_colon' => '',
 			'menu_name' => 'Galleries'
 	);
-	
+
     //// ARGUMENTS
 		$args = array(
-		
+
 			'labels' => $labels,
 			'public' => true,
 			'publicly_queryable' => true,
-			'show_ui' => true, 
-			'show_in_menu' => true, 
+			'show_ui' => true,
+			'show_in_menu' => true,
 			'query_var' => true,
 			'rewrite' => true,
 			'capability_type' => 'post',
-			'has_archive' => true, 
+			'has_archive' => true,
 			'hierarchical' => false,
 			'menu_position' => 7,
 			'menu_icon' => '',
 			'supports' => array('title', 'editor', 'thumbnail','comments','revisions','author')
-		  
-		);  
-  
+
+		);
+
     	//// REGISTERS IT
 		register_post_type('gallery', $args);
-}  
+}
 // Adds Custom Post Type*/
-add_action('init', 'unpress_gallery_register'); 
+add_action('init', 'unpress_gallery_register');
 
 // Taxonomy Categories
 function unpress_gallery_categories(){
-	register_taxonomy(  
-	'gallery-categories', 'gallery',  
-	array(  
-		'hierarchical' => true,  
+	register_taxonomy(
+	'gallery-categories', 'gallery',
+	array(
+		'hierarchical' => true,
 		'labels' => array(
 			'name' => __( 'Gallery Categories', 'favethemes' ),
 			'singular_name' => __( 'Category', 'favethemes' ),
@@ -67,12 +67,12 @@ function unpress_gallery_categories(){
 			'new_item_name' => __( 'New Category Name', 'favethemes' ),
 			'separate_items_with_commas' => __( 'Separate Categories With Commas', 'favethemes' ),
 			'add_or_remove_items' => __( 'Add or Remove Category', 'favethemes' ),
-			'choose_from_most_used' => __( 'Choose From Most Used Categories', 'favethemes' ),  
-			'parent' => __( 'Parent Category', 'favethemes' )      	
+			'choose_from_most_used' => __( 'Choose From Most Used Categories', 'favethemes' ),
+			'parent' => __( 'Parent Category', 'favethemes' )
 			),
-		'query_var' => true,  
-		'rewrite' => true  
-		)  
+		'query_var' => true,
+		'rewrite' => true
+		)
 	);
 }
 add_action( 'init', 'unpress_gallery_categories' );
@@ -85,10 +85,10 @@ function unpress_gallerycategory() {
 }
 // Taxonomy Tags
 function unpress_gallery_tags(){
-	register_taxonomy(  
-	'gallery-tags', 'gallery',  
-	array(  
-		'hierarchical' => false,  
+	register_taxonomy(
+	'gallery-tags', 'gallery',
+	array(
+		'hierarchical' => false,
 		'labels' => array(
 			'name' => __( 'Gallery Tags', 'favethemes' ),
 			'singular_name' => __( 'Tag', 'favethemes' ),
@@ -101,12 +101,12 @@ function unpress_gallery_tags(){
 			'new_item_name' => __( 'New Tag Name', 'favethemes' ),
 			'separate_items_with_commas' => __( 'Separate Tags With Commas', 'favethemes' ),
 			'add_or_remove_items' => __( 'Add or Remove Tag', 'favethemes' ),
-			'choose_from_most_used' => __( 'Choose From Most Used Tags', 'favethemes' ),  
-			'parent' => __( 'Parent Tag', 'favethemes' )      	
+			'choose_from_most_used' => __( 'Choose From Most Used Tags', 'favethemes' ),
+			'parent' => __( 'Parent Tag', 'favethemes' )
 			),
-		'query_var' => true,  
-		'rewrite' => true  
-		)  
+		'query_var' => true,
+		'rewrite' => true
+		)
 	);
 }
 add_action( 'init', 'unpress_gallery_tags' );
@@ -123,21 +123,21 @@ function unpress_gallerytags() {
 // start Gallery columns
 
 // Change the columns for the edit CPT screen
-function unpress_gallery_change_columns( $cols ) {
-	$cols = array(
-			"cb" => '<input type="checkbox" />',
-			"title" => __( "Gallery Title", "favethemes" ),
-			"gallery_category" => __( "Category", "favethemes" ),
-			"gallery_image" => __( "Picture", "favethemes" ),
-			"date" => __( "Last Updated", "favethemes" ),
-		);
-	return $cols;
-}
-add_filter( "manage_gallery_posts_columns", "unpress_gallery_change_columns" );
+// function unpress_gallery_change_columns( $cols ) {
+// 	$cols = array(
+// 			"cb" => '<input type="checkbox" />',
+// 			"title" => __( "Gallery Title", "favethemes" ),
+// 			"gallery_category" => __( "Category", "favethemes" ),
+// 			"gallery_image" => __( "Picture", "favethemes" ),
+// 			"date" => __( "Last Updated", "favethemes" ),
+// 		);
+// 	return $cols;
+// }
+// add_filter( "manage_gallery_posts_columns", "unpress_gallery_change_columns" );
 
 function unpress_gallery_custom_columns( $column, $post_id ) {
 	global $ft_option;
-	
+
 	switch ( $column ) {
 		case "gallery_category":
 			$gallery_category = wp_get_post_terms($post_id, 'gallery-categories', array("fields" => "all"));
@@ -152,7 +152,7 @@ function unpress_gallery_custom_columns( $column, $post_id ) {
 		case "gallery_image":
 			the_post_thumbnail(array( 150,150));
 		break;
-		
+
 	}
 }
 add_action( "manage_posts_custom_column", "unpress_gallery_custom_columns", 10, 2 );
@@ -162,14 +162,14 @@ add_action( "manage_posts_custom_column", "unpress_gallery_custom_columns", 10, 
 
 
 /*===========================================
-  Filters  
+  Filters
 ==============================================*/
 
 // Categories
 function unpress_restrict_gallery_by_category() {
 	global $typenow;
-	$post_type = 'gallery'; 
-	$taxonomy = 'gallery-categories'; 
+	$post_type = 'gallery';
+	$taxonomy = 'gallery-categories';
 	if ($typenow == $post_type) {
 		$selected = isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '';
 		$info_taxonomy = get_taxonomy($taxonomy);
@@ -188,8 +188,8 @@ add_action('restrict_manage_posts', 'unpress_restrict_gallery_by_category');
 
 function unpress_convert_gallery_id_to_term_in_query($query) {
 	global $pagenow;
-	$post_type = 'gallery'; 
-	$taxonomy = 'gallery-categories'; 
+	$post_type = 'gallery';
+	$taxonomy = 'gallery-categories';
 	$q_vars = &$query->query_vars;
 	if ($pagenow == 'edit.php' && isset($q_vars['post_type']) && $q_vars['post_type'] == $post_type && isset($q_vars[$taxonomy]) && is_numeric($q_vars[$taxonomy]) && $q_vars[$taxonomy] != 0) {
 		$term = get_term_by('id', $q_vars[$taxonomy], $taxonomy);
