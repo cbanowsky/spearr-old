5c0f5fbcbb7f6b2bcaabbf43933486842b2b74a0
Cameron Banowsky <cameron@hack.cd>
2015-12-30 11:21:33 -0800

    change scripts

--- functions.php
+++ functions.php
@@ -442,8 +442,8 @@ function unpress_share_button(){
 
 	if($ft_option["single_social"]=="1"): ?>
     <div class="post-sharing-wrap pull-right">
-        <a class="btn-icon btn btn-post-share" data-toggle="tooltip" title="SHARE" id="show-inline" href="#">
-            <i data-toggle="tooltip" title="share" class="fa fa-plus"></i>
+        <a class="btn-icon btn btn-post-share" style="hover:none" data-toggle="tooltip" title="SHARE" id="show-inline" href="#">
+            <i class="fa fa-plus"></i>
         </a>
     </div>
     <?php endif;
