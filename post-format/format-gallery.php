c5356d926413ca561243819bd48ef9b4038776c0
Cameron Banowsky <cameron@hack.cd>
2016-01-22 15:41:11 -0800

    image

--- post-format/format-gallery.php
+++ post-format/format-gallery.php
@@ -14,7 +14,7 @@ global $ft_option;
     <?php
     if ( have_posts() ) :
       while ( have_posts() ) : the_post();
-      $post_image = wp_get_attachment_image_src( get_post_image($post->ID), 'full');
+      $post_image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');
 
       // Set post view
       fave_setPostViews(get_the_ID());
